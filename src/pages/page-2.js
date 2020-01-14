232e49f9a52bfce4579f4425514a46d012a3b9f9
Ryan Leach <leachrp@comcast.net>
2020-01-13 20:43:18 -0800

    Add plugins, modify pages

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,13 +4,17 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { GlobalLayout } from "../styles/styles"
+
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  <GlobalLayout>
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  </GlobalLayout>
 )
 
 export default SecondPage
---
+++ src/styles/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+const GlobalLayout = styled.body`
+  background-color: coral;
+`
+export default GlobalLayout
